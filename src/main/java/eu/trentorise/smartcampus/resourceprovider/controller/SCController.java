d8155a960b52a4274d8633a71b66c0987d414aa3
mirkoperillo <mirko.perillo@gmail.com>
2013-07-02 17:50:27 +0200

    added temporary retrieveuser 1.1

--- src/main/java/eu/trentorise/smartcampus/resourceprovider/controller/SCController.java
+++ src/main/java/eu/trentorise/smartcampus/resourceprovider/controller/SCController.java
@@ -11,35 +11,48 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
-
+import eu.trentorise.smartcampus.ac.provider.AcService;
+import eu.trentorise.smartcampus.ac.provider.filters.AcProviderFilter;
+import eu.trentorise.smartcampus.ac.provider.model.User;
 import eu.trentorise.smartcampus.resourceprovider.jdbc.JdbcResourceServices;
-
-
-
 
 @Controller
 public class SCController {
-	
+
 	@Autowired
 	DataSource dataSource;
-	
-	
+
+	@Autowired
+	private AcService acService;
+
 	private static final Logger logger = Logger.getLogger(SCController.class);
-	protected String retrieveAppName(HttpServletRequest request)
-			 {
+
+	protected String retrieveAppId(HttpServletRequest request) {
 		try {
-			Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-	
-			Map<String,String> x=new JdbcResourceServices(dataSource).loadAddInfoByToken(auth.getPrincipal().toString());
-			
+			Authentication auth = SecurityContextHolder.getContext()
+					.getAuthentication();
+
+			Map<String, String> x = new JdbcResourceServices(dataSource)
+					.loadAddInfoByToken(auth.getPrincipal().toString());
+
 			return x.get("name");
-		
+
 		} catch (Exception e) {
 			logger.error("Exception checking token");
-			
+
 		}
 		return null;
 	}
 
+	protected User retrieveUser(HttpServletRequest request) {
+		try {
+			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
+			return acService.getUserByToken(token);
+		} catch (Exception e) {
+			logger.error("Exception checking token");
+		}
+
+		return null;
+	}
 
 }
